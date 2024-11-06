65d92f8cb29a0517b77eade8bfd8ba69518ae7e7
Radih P Paraguas Jr <radihjrprgs@gmail.com>
2024-11-06 17:50:00 +0800

    fix image locations

--- src/services/gameService.ts
+++ src/services/gameService.ts
@@ -9,67 +9,67 @@ export const fetchGames = async ({ category, searchKey }: FetchGameProps) => {
   const games: GameData[] = [
     {
       name: "Sugar Rsuh",
-      imgSrc: "src/assets/games/Group 70.webp",
+      imgSrc: "/assets/games/Group 70.webp",
       category: GameCategory.START,
     },
     {
       name: "Shrolin Crew",
-      imgSrc: "src/assets/games/Group 75.webp",
+      imgSrc: "/assets/games/Group 75.webp",
       category: GameCategory.START,
     },
     {
       name: "Big Bad Wolf",
-      imgSrc: "src/assets/games/Group 72.webp",
+      imgSrc: "/assets/games/Group 72.webp",
       category: GameCategory.START,
     },
     {
       name: "Book of Egypt",
-      imgSrc: "src/assets/games/Group 83.webp",
+      imgSrc: "/assets/games/Group 83.webp",
       category: GameCategory.START,
     },
     {
       name: "Pirates Power",
-      imgSrc: "src/assets/games/Group 84.webp",
+      imgSrc: "/assets/games/Group 84.webp",
       category: GameCategory.START,
     },
     {
       name: "Crocodile Blitz Extreme",
-      imgSrc: "src/assets/games/Group 85.webp",
+      imgSrc: "/assets/games/Group 85.webp",
       category: GameCategory.START,
     },
     {
       name: "Crocodile Blitz Extreme FB",
-      imgSrc: "src/assets/games/Group 85.webp",
+      imgSrc: "/assets/games/Group 85.webp",
       category: GameCategory.START,
     },
     {
       name: "Anaconda Wild 2: Powerplay Jackpot",
-      imgSrc: "src/assets/games/Group 86.webp",
+      imgSrc: "/assets/games/Group 86.webp",
       category: GameCategory.JACKPOTS,
     },
     {
       name: "Maya Jackpot",
-      imgSrc: "src/assets/games/Group 87.webp",
+      imgSrc: "/assets/games/Group 87.webp",
       category: GameCategory.JACKPOTS,
     },
     {
       name: "Beachlife",
-      imgSrc: "src/assets/games/Group 88.webp",
+      imgSrc: "/assets/games/Group 88.webp",
       category: GameCategory.START,
     },
     {
       name: "Azteca: Powerplay Jackpot",
-      imgSrc: "src/assets/games/Group 94.webp",
+      imgSrc: "/assets/games/Group 94.webp",
       category: GameCategory.JACKPOTS,
     },
     {
       name: "Inca Jackpot",
-      imgSrc: "src/assets/games/sw_ijp.webp.webp",
+      imgSrc: "/assets/games/sw_ijp.webp.webp",
       category: GameCategory.JACKPOTS,
     },
     {
       name: "Empire Treasures: Pride of Persia",
-      imgSrc: "src/assets/games/sw_ijp.webp.webp",
+      imgSrc: "/assets/games/sw_ijp.webp.webp",
       category: GameCategory.LIVE,
     },
   ];
